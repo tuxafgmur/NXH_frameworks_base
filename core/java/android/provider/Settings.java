0e9a93240aaaabc2bc9931060a502a76d20952ca
Altaf-Mahdi <altaf.mahdi@gmail.com>
2016-09-29 20:37:53 +0200

    Quick settings: Option to disable brightness slider (1/2)
    before:
    https://www.dropbox.com/s/r4m1ytiwbdbj1xw/Screenshot_2015-01-12-21-29-55.png?dl=0
    
    after:
    https://www.dropbox.com/s/lvswhsubux548xh/Screenshot_2015-01-12-21-27-31.png?dl=0
    
    Signed-off-by: Altaf-Mahdi <altaf.mahdi@gmail.com>
    
    AICPfy:
    - Bring it to N by @LorDClockaN
    - Remove SystemUI tuner bits
    
    Change-Id: I98a28e99e78ee4d8bb4f03bc53e867723d62b052

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3774,6 +3774,12 @@ public final class Settings {
          */
         public static final String DOUBLE_TAP_SLEEP_ANYWHERE = "double_tap_sleep_anywhere";
 
+        /** Whether to show the brightness slider in quick settings panel.
+         *
+         * @hide
+         */
+        public static final String QS_SHOW_BRIGHTNESS_SLIDER = "qs_show_brightness_slider";
+
         /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
