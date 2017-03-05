e29b18c7888282e1a7a6832f5dd7afbadf54e890
LorDClockaN <lordclockan@gmail.com>
2017-03-05 08:29:34 +0100

    LuckyPatcher: Add dialog on every boot
    Change-Id: I35a80e617ff5fc510b7dda12aa9d7d5b58e0d857
    
    * Xenonify
    
    Signed-off-by: dadi11 <dadi.sikorski@gmail.com>

---
+++ core/java/com/android/internal/util/xenonhd/XenonUtils.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright (C) 2014 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.internal.util.xenonhd;
+
+import android.app.ActivityManager;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
+import android.content.res.Resources;
+import android.hardware.camera2.CameraAccessException;
+import android.hardware.camera2.CameraCharacteristics;
+import android.hardware.camera2.CameraManager;
+import android.net.ConnectivityManager;
+import android.util.Log;
+
+import java.util.List;
+
+import java.util.Locale;
+
+public class XenonUtils {
+
+    private static final String TAG = "XenonUtils";
+
+    public static boolean isLuckyPatcherInstalled(final Context context) {
+        boolean mluckyPatcherInstalled = false;
+        try {
+            mluckyPatcherInstalled = (context.getPackageManager().getPackageInfo(
+                    "com.android.vending.billing.InAppBillingService.LOCK", 0).versionCode > 0);
+        } catch (PackageManager.NameNotFoundException e) {
+        }
+        return mluckyPatcherInstalled;
+    }
+}
