1a40daad926d810b0168bf3edb80993dc0cac0fb
Steve Kondik <steve@cyngn.com>
2017-11-05 16:37:52 +0100

    SystemUI: Adding LiveDisplay tile
    Change-Id: I53d492e5cb9998268104d5750705aa5ed55d9658

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/QSTileHost.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/QSTileHost.java
@@ -60,6 +60,7 @@ import com.android.systemui.qs.tiles.HotspotTile;
 import com.android.systemui.qs.tiles.ImageTile;
 import com.android.systemui.qs.tiles.ImeTile;
 import com.android.systemui.qs.tiles.IntentTile;
+import com.android.systemui.qs.tiles.LiveDisplayTile;
 import com.android.systemui.qs.tiles.LocationTile;
 import com.android.systemui.qs.tiles.LockscreenToggleTile;
 import com.android.systemui.qs.tiles.NfcTile;
@@ -470,6 +471,7 @@ public class QSTileHost implements QSTile.Host, Tunable {
         else if (tileSpec.equals("work")) return new WorkModeTile(this);
         else if (tileSpec.equals("rotation")) return new RotationLockTile(this);
         else if (tileSpec.equals("flashlight")) return new FlashlightTile(this);
+        else if (tileSpec.equals("livedisplay")) return new LiveDisplayTile(this);
         else if (tileSpec.equals("location")) return new LocationTile(this);
         else if (tileSpec.equals("profiles")) return new ProfilesTile(this);
         else if (tileSpec.equals("cast")) return new CastTile(this);
